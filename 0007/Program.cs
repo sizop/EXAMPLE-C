cc9b8be2f0c148afa6e52e040371129f92c2172a
Dmirty <9048881951@mail.ru>
2022-08-15 10:19:15 +0500

    15.08.2022

--- 0007/Program.cs
+++ 0007/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// #1
+/*
+Console.Write($"Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for (int i = 1; i <= B; i++)
+{
+    result *= A;
+}
+Console.WriteLine(result);
+*/
+
+// #2
+
+Console.Write($"Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+for (int i = number; i>0; i/=10)
+{
+	result = result + number % 10;
+}
+Console.WriteLine($"Сумма цифр составляет: {result}");
